588b69ae77c08b4f5b130ff6baf8932ec5260775
Vinicius jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2017-11-06 15:55:49 -0300

    Extend Counting

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,39 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+		if (leftIndex < rightIndex && array != null && leftIndex >= 0 && rightIndex < array.length && rightIndex > 0) {
+			int menor = array[leftIndex];
+			int maior = array[leftIndex];
 
+			for (int i = leftIndex; i < rightIndex + 1; i++) {
+				if (array[i] > maior) {
+					maior = array[i];
+				}
+				if (array[i] < menor) {
+					menor = array[i];
+				}
+			}
+			Integer[] tempArray = new Integer[maior - menor + 1];
+
+			for (int i = 0; i < tempArray.length; i++) {
+				tempArray[i] = 0;
+			}
+
+			for (int i = leftIndex; i < rightIndex + 1; i++) {
+				tempArray[array[i] - menor] += 1;
+			}
+
+			int i = leftIndex;
+			int j = leftIndex;
+
+			while (i < tempArray.length) {
+				while (tempArray[i] != 0) {
+					array[j] = i + menor;
+					j += 1;
+					tempArray[i] -= 1;
+				}
+				i += 1;
+			}
+		}
+	}
 }
