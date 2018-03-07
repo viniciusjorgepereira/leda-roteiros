8d6b1d3cfc016810cc1fc9b605a25240d1130308
Vinicius jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-03-07 17:54:56 -0300

    Prova

--- PP3-01-Prova-SkipListFromBST-environment/src/main/java/adt/skipList/fromBST/SkipListFromBSTImpl.java
+++ PP3-01-Prova-SkipListFromBST-environment/src/main/java/adt/skipList/fromBST/SkipListFromBSTImpl.java
@@ -8,39 +8,39 @@ import adt.skipList.SkipListNode;
 
 public class SkipListFromBSTImpl extends SkipListImpl<Integer> implements SkipListFromBST<Integer> {
 
-   public SkipListFromBSTImpl(int maxHeight) {
-      super(maxHeight);
-   }
+	public SkipListFromBSTImpl(int maxHeight) {
+		super(maxHeight);
+	}
 
-   public void importFromBST(BST<Integer> bst) {
-      Integer[] array = bst.preOrder();
+	public void importFromBST(BST<Integer> bst) {
+		Integer[] array = bst.preOrder();
 
-      int altura = bst.height();
-      this.root = resetSkip(this.root, altura);
+		int altura = bst.height();
+		this.root = resetSkip(this.root, altura);
 
-      for (int i = 0; i < array.length; i++) {
-         BTNode<Integer> no = bst.search(array[i]);
-         BTNode<Integer> parent = no.getParent();
+		for (int i = 0; i < array.length; i++) {
+			BTNode<Integer> no = bst.search(array[i]);
+			BTNode<Integer> raiz = bst.getRoot();
 
-         int alt = alturaNoParent(no, parent);
+			int alt = alturaNoParent(no, raiz);
 
-         insert(parent.getData(), parent.getData(), alt);
-      }
+			insert(raiz.getData(), raiz.getData(), alt);
+		}
 
-   }
+	}
 
-   private SkipListNode<Integer> resetSkip(SkipListNode<Integer> root, int altura) {
-      return new SkipListNode<Integer>(Integer.MIN_VALUE, altura, null);
-   }
+	private SkipListNode<Integer> resetSkip(SkipListNode<Integer> root, int altura) {
+		return new SkipListNode<Integer>(Integer.MIN_VALUE, altura, null);
+	}
 
-   private int alturaNoParent(BTNode<Integer> no, BTNode<Integer> parent) {
-      int alt = 1;
-      BSTNode<Integer> node = (BSTNode<Integer>) no;
-      if (node.getParent() != parent) {
-         node = (BSTNode<Integer>) node.getParent();
-         alt += 1;
-      }
-      return alt;
-   }
+	private int alturaNoParent(BTNode<Integer> no, BTNode<Integer> parent) {
+		int alt = 1;
+		BSTNode<Integer> node = (BSTNode<Integer>) no;
+		if (node.getParent() != parent) {
+			node = (BSTNode<Integer>) node.getParent();
+			alt += 1;
+		}
+		return alt;
+	}
 
 }
