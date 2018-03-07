f74b52e7a27716b6e1630a755e0f3a3642a69b36
Vinicius jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-03-07 17:51:37 -0300

    Prova

--- PP3-01-Prova-SkipListFromBST-environment/src/main/java/adt/skipList/fromBST/SkipListFromBSTImpl.java
+++ PP3-01-Prova-SkipListFromBST-environment/src/main/java/adt/skipList/fromBST/SkipListFromBSTImpl.java
@@ -1,54 +1,46 @@
 package adt.skipList.fromBST;
 
-import java.util.Arrays;
-
 import adt.bst.BST;
-import adt.bst.BSTImpl;
 import adt.bst.BSTNode;
+import adt.bt.BTNode;
 import adt.skipList.SkipListImpl;
 import adt.skipList.SkipListNode;
 
 public class SkipListFromBSTImpl extends SkipListImpl<Integer> implements SkipListFromBST<Integer> {
 
-	public SkipListFromBSTImpl(int maxHeight) {
-		super(maxHeight);
-	}
+   public SkipListFromBSTImpl(int maxHeight) {
+      super(maxHeight);
+   }
 
-	public void importFromBST(BST<Integer> bst) {
-		Integer[] array = bst.preOrder();
+   public void importFromBST(BST<Integer> bst) {
+      Integer[] array = bst.preOrder();
 
-		int altura = bst.height();
-		this.root = resetSkip(this.root, altura);
+      int altura = bst.height();
+      this.root = resetSkip(this.root, altura);
 
-		for (int i = 0; i < array.length; i++) {
-			BSTNode<Integer> raiz = (BSTNode<Integer>) bst.getRoot();
-			int alt = bst.height();
-			int key = raiz.getData();
-			bst.remove(key);
-			insert(key, array[i], alt);
-		}
+      for (int i = 0; i < array.length; i++) {
+         BTNode<Integer> no = bst.search(array[i]);
+         BTNode<Integer> parent = no.getParent();
 
-	}
+         int alt = alturaNoParent(no, parent);
 
-	private SkipListNode<Integer> resetSkip(SkipListNode<Integer> root, int altura) {
-		return new SkipListNode<Integer>(Integer.MIN_VALUE, altura, null);
-	}
+         insert(parent.getData(), parent.getData(), alt);
+      }
 
-	public static void main(String[] args) {
-		BSTImpl bst = new BSTImpl<>();
+   }
 
-		SkipListFromBSTImpl skip = new SkipListFromBSTImpl(5);
-		skip.importFromBST(bst);
+   private SkipListNode<Integer> resetSkip(SkipListNode<Integer> root, int altura) {
+      return new SkipListNode<Integer>(Integer.MIN_VALUE, altura, null);
+   }
 
-		bst.insert(3);
-		bst.insert(8);
-		bst.insert(25);
-		bst.insert(47);
-
-		System.out.println(Arrays.toString(bst.order()));
-
-		System.out.println(Arrays.toString(skip.toArray()));
-
-	}
+   private int alturaNoParent(BTNode<Integer> no, BTNode<Integer> parent) {
+      int alt = 1;
+      BSTNode<Integer> node = (BSTNode<Integer>) no;
+      if (node.getParent() != parent) {
+         node = (BSTNode<Integer>) node.getParent();
+         alt += 1;
+      }
+      return alt;
+   }
 
 }
